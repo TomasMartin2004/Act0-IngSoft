cd7f213295c3c52c47bb6f13254f8694a7a29568
Tomas Martin <tomassmartin04@gmail.com>
2025-04-13 15:47:29 -0300

    [Parte 4.3] usando clases en views

--- Act0/polls/views.py
+++ Act0/polls/views.py
@@ -1,29 +1,29 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
-from django.http import Http404
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+from .models import Choice, Question
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
@@ -44,7 +44,3 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
     
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
